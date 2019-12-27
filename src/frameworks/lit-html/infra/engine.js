11926e9dec748de5cafdd2277c73224130bedb8d
David <desmaisons_david@hotmail.com>
2019-12-27 00:39:45 -0300

    Prototyping time-travel

--- src/frameworks/lit-html/infra/engine.js
+++ src/frameworks/lit-html/infra/engine.js
@@ -1,4 +1,13 @@
-import {render} from 'lit-html';
+import { render } from 'lit-html';
+import { connectViaExtension, extractState } from 'remotedev';
+
+const remoteDev = connectViaExtension();
+
+function getSendableState(state) {
+  const stateToSend = { ...state };
+  delete stateToSend.commands;
+  return stateToSend;
+}
 
 class UiEngine {
   constructor(element, template) {
@@ -7,9 +16,12 @@ class UiEngine {
   }
 
   initialRender(state) {
+    remoteDev.init(getSendableState(state), { name: 'Uapps' });
     return new View({ ...state }, this._element, this._template);
   }
 }
+
+let currentView;
 
 class View {
   constructor(state, element, template) {
@@ -17,13 +29,30 @@ class View {
     this._element = element;
     this._template = template;
     render(template(this._state), element);
+    currentView = this;
   }
 
   update(updater) {
     const newState = { ...this._state };
     updater(newState);
+    remoteDev.send("blabla", getSendableState(newState));
     return new View(newState, this._element, this._template);
   }
+
+  fullUpdate(newState){
+    const {commands} = this._state;
+    this._state = { ...newState, commands};
+    render(this._template(this._state), this._element);
+  }
 }
+
+remoteDev.subscribe(message => {
+  const state = extractState(message);
+  if (!state) {
+    return;
+  }
+  currentView.fullUpdate(state);
+});
+
 
 export { UiEngine };
